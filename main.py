10c1c98c5fd827cc340a7e4e7a1ac13a1f7e4bd3
carolmetanis3105 <carol.metanis@gmail.com>
2025-11-26 15:08:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-from string_utils.py import split_before_uppercases
-from string_utils.py import split_at_digit
-from string_utils.py import count_atoms_in_molecule
-from string_utils.py import parse_chemical_reaction
-from string_utils.py import count_atoms_in_reaction
-from equation_utils.py import build_equations
-from equation_utils.py import my_solve
-
+import string_utils as aa
+import equation_utils as bb
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
     # 1.parse reaction
-    reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
-    product_atoms = count_atoms_in_reaction(products)
+    reactants, products = aa.parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
+    reactant_atoms = aa.count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+    product_atoms = aa.count_atoms_in_reaction(products)
 
     # 2.build equation and solve
-    equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    coefficients = my_solve(equations, coefficients) + [1]
+    equations, coefficients = bb.build_equations(reactant_atoms, product_atoms)
+    coefficients = bb.my_solve(equations, coefficients) + [1]
 
     return coefficients # [1/3, 1, 2/3, 1]
 
